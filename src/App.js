2ac77c7664a86c322a60a831315877f49e53c6f5
Alan Chuan <alan@Alans.local>
2021-01-03 00:18:00 -0600

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMinusSquare, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+import SetTimer from "./components/SetTimer";
+library.add(faMinusSquare, faPlusSquare);
 
 class App extends Component {
   render() {
-    return <div></div>;
+    const breakProps = {
+      title: "Break Length",
+      value: 5,
+      handleDecrease: this.handleDecrement,
+      handleIncrease: this.handleIncrement
+    };
+
+    const sessionProps = {
+      title: "Session Length",
+      value: 25,
+      handleDecrease: this.handleDecrement,
+      handleIncrease: this.handleIncrement
+    };
+    return (
+      <div>
+        <div>
+          <SetTimer {...breakProps} />
+          <SetTimer {...sessionProps} />
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/components/SetTimer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function SetTimer(props) {
+  return (
+    <div className="timer-container">
+      <h1>{props.title}</h1>
+      <div>
+        <button onClick={props.handleIncrement}>
+          {" "}
+          <FontAwesomeIcon icon="minus-square" />
+        </button>
+        <span>{props.value}</span>
+        <button>
+          <FontAwesomeIcon icon="plus-square" />
+        </button>
+      </div>
+    </div>
+  );
+}
