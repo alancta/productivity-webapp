42bee9549680e8f964475c7029417e6c2e3bd547
Riza Hassan <ruhulruzbihan@gmail.com>
2021-01-12 01:29:42 -0600

    Added header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function Header(){
+    return(
+        <div style={{height: "50px"}} class="bg-primary">
+            <div class="  text-center fs-2">POMODORO TIMER</div>
+        </div>
+    )
+}
+
+export default Header;
