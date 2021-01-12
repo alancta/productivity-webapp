c78fa359cb668c1cd81a5fe0bd2691f7cffb9f8c
Riza Hassan <ruhulruzbihan@gmail.com>
2021-01-12 01:29:30 -0600

    Added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function Footer(){
+    return(
+        <div class="position-fixed mb-0"><footer class=" text-center">
+        <div>© 2020 Copyright</div>
+    </footer></div>
+
+        
+    )
+}
+
+export default Footer;
